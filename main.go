7a187099a5bf8c4f7cee7b39cfb65cebd953f620
Churchill <churchillrimamskep@gmail.com>
2025-01-06 10:00:00 +0100

    appended to the slice

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main(){
 	fmt.Println("Hello world")
 	letters := []string{"a", "b", "c"}
-	// letters =  append(letters, "d")
+	letters =  append(letters, "d")
 	fmt.Println(letters)
 }
