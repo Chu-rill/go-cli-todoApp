98381a41ca5f99c514debbb84b8c1aff5e7c4dc6
Churchill <churchillrimamskep@gmail.com>
2025-01-08 11:54:22 +0100

    got the table to show in the terminal with the completed task

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
-import "fmt"
-
 func main(){
-	fmt.Println("Hello world")
-	letters := []string{"a", "b", "c"}
-	letters =  append(letters, "d")
-	fmt.Println(letters)
+	todos := Todos{}
+	todos.add("Learn Go")
+	todos.add("Write a blog post")
+	todos.add("Read a book")
+	todos.toggle(0)
+	todos.print()
 }
